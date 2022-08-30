503410229c04f6ae6d06abc19938df54f7177827
Olya <ponomaryova.olya.y@gmail.com>
2022-08-30 18:15:17 +0200

    Create app container, add home screen

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import React from "react";
+import Button from "@mui/material/Button";
+import Box from "@mui/material/Box";
 import "./App.css";
 
 export default function App() {
   return (
-    <div className="App">
-      <header className="App-header"></header>
-    </div>
+    <Box className="App" sx={{ height: "100%" }}>
+      <Box
+        className="NavigationButtons"
+        sx={{
+          display: "flex",
+          flexWrap: "wrap",
+          alignItems: "center",
+          justifyContent: "center",
+          flexDirection: "column",
+          height: "100%",
+        }}
+      >
+        <Button variant="outlined" size="large">
+          Практика
+        </Button>
+        <Button variant="outlined" size="large">
+          Словник
+        </Button>
+      </Box>
+    </Box>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import Container from "@mui/material/Container";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Container maxWidth="sm" sx={{ height: "100%" }}>
+      <App sx={{ bgcolor: "#cfe8fc", height: "100%" }} />
+    </Container>
   </React.StrictMode>
 );
 
