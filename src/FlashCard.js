0262a0b0a8f67b91a460db4959880493bbec4873
Olya <ponomaryova.olya.y@gmail.com>
2022-11-09 15:21:58 +0100

    Add flip animation to flashcard

--- src/FlashCard.js
+++ src/FlashCard.js
@@ -7,62 +7,95 @@ import CardContent from "@mui/material/CardContent";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
-
-const card = (
-  <React.Fragment>
-    <CardContent>
-      <Typography
-        sx={{ fontSize: 18, mt: "10px", ml: "10px" }}
-        variant="h6"
-        gutterBottom
-      >
-        PL
-      </Typography>
-      <Typography
-        variant="h5"
-        component="div"
-        sx={{ fontSize: 40, mt: "25px", textAlign: "center" }}
-      >
-        Cześć
-      </Typography>
-    </CardContent>
-    <CardActions sx={{ justifyContent: "center" }}>
-      <Button
-        size="small"
-        sx={{
-          fontSize: 10,
-          mb: "20px",
-        }}
-      >
-        Tłumaczenie
-      </Button>
-    </CardActions>
-  </React.Fragment>
-);
+import "./Flashcard.css";
 
 export default function FlashCard() {
+  const [showBack, setShowBack] = React.useState(false);
+
+  const flipCard = () => {
+    setShowBack(true);
+  };
+  const showFront = () => {
+    setShowBack(false);
+  };
+
+  const cardFront = (
+    <React.Fragment>
+      <CardContent>
+        <Typography
+          sx={{ fontSize: 18, mt: "10px", ml: "10px" }}
+          variant="h6"
+          gutterBottom
+        >
+          PL
+        </Typography>
+        <Typography
+          variant="h5"
+          component="div"
+          sx={{ fontSize: 40, mt: "25px", textAlign: "center" }}
+        >
+          Cześć
+        </Typography>
+      </CardContent>
+    </React.Fragment>
+  );
+
+  const cardBack = (
+    <React.Fragment>
+      <CardContent>
+        <Typography
+          sx={{ fontSize: 18, mt: "10px", ml: "10px" }}
+          variant="h6"
+          gutterBottom
+        >
+          UA
+        </Typography>
+        <Typography
+          variant="h5"
+          component="div"
+          sx={{ fontSize: 40, mt: "25px", textAlign: "center", mb: "40px" }}
+        >
+          Привіт
+        </Typography>
+      </CardContent>
+    </React.Fragment>
+  );
+
   return (
     <Container className="FlashCard">
-      <Box
-        sx={{
-          width: "100%",
-        }}
-      >
-        <Card
-          variant="outlined"
-          sx={{
-            mt: "120px",
-            borderRadius: "16px",
-            border: "3px solid #1976d2",
-          }}
-        >
-          {card}
-        </Card>
-        <Box sx={{ fontSize: "60px", textAlign: "right" }}>
-          <Button variant="text">
-            <ArrowForwardIcon sx={{ fontSize: "30px" }} />
-          </Button>
+      <Box className="FlashCardContainer flip">
+        <Box className="FlipCard">
+          <Card className="FlipCardFront" sx={{ borderRadius: "16px" }}>
+            {cardFront}
+            <CardActions sx={{ justifyContent: "center" }}>
+              <Button
+                size="small"
+                sx={{
+                  fontSize: 10,
+                  mb: "20px",
+                }}
+                onClick={flipCard}
+                onClose={showFront}
+              >
+                Tłumaczenie
+              </Button>
+            </CardActions>
+          </Card>
+
+          <Card
+            className="FlipCardBack"
+            open={showBack}
+            onClose={showFront}
+            sx={{ borderRadius: "16px" }}
+          >
+            {cardBack}
+          </Card>
         </Box>
+      </Box>
+      <Box sx={{ fontSize: "60px", textAlign: "right" }}>
+        <Button variant="text">
+          <ArrowForwardIcon sx={{ fontSize: "30px" }} />
+        </Button>
       </Box>
     </Container>
   );
