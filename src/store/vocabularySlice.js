cb76f1a21137d48b0677c79c8d49df9e39c9507d
Olya <ponomaryova.olya.y@gmail.com>
2022-11-14 18:02:15 +0100

    Use local storage

--- src/store/vocabularySlice.js
+++ src/store/vocabularySlice.js
@@ -3,41 +3,31 @@ import { createSlice } from "@reduxjs/toolkit";
 export const vocabularySlice = createSlice({
   name: "vocabulary",
   initialState: {
-    data: [
-      {
-        id: 1,
-        value_ua: "pies",
-        value_pl: "собака",
-        description: "",
-      },
-      {
-        id: 2,
-        value_ua: "kot",
-        value_pl: "кіт",
-        description: "",
-      },
-      {
-        id: 3,
-        value_ua: "tygrys",
-        value_pl: "тигр",
-        description: "",
-      },
-      {
-        id: 4,
-        value_ua: "lew",
-        value_pl: "лев",
-        description: "",
-      },
-    ],
+    data: getFromLocalStorage(),
   },
   reducers: {
     addWord: (state, action) => {
       // @TODO generate new id
       state.data.push(action.payload);
+      saveToLocalStorage(state.data);
       return state;
     },
   },
 });
+
+function saveToLocalStorage(data) {
+  localStorage.setItem("vocabulary", JSON.stringify(data));
+}
+
+function getFromLocalStorage() {
+  const stored = localStorage.getItem("vocabulary");
+  const parsed = JSON.parse(stored);
+  if (parsed !== null) {
+    return parsed;
+  } else {
+    return [];
+  }
+}
 
 export const { addWord } = vocabularySlice.actions;
 
